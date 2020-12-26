909c4695fc414f8348e6eea6ab64877419b6e925
Topik <topik@topik.tech>
2020-12-25 18:22:31 -0600

    Added styles for the BulletPoint component

---
+++ src/components/BulletPoint/BulletPoint.js
@@ -0,0 +1,21 @@
+import styled from "styled-components"
+
+export const BulletContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`
+export const Circle = styled.div`
+  height: 17px;
+  width: 17px;
+  border-radius: 50%;
+  background-color: #e5e5e5;
+`
+export const Content = styled.p`
+  font-family: "Pier Sans", sans-serif;
+  font-size: 30px;
+  text-shadow: 0px 0px 7px rgba(0, 0, 0, 0.25);
+  color: white;
+  margin-left: 10px;
+  font-weight: 300;
+`
