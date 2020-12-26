ffc6df5c3e52373c99dde10733578b136286f96a
Topik <topik@topik.tech>
2020-12-25 18:29:08 -0600

    Created the Meet The Team component

---
+++ src/components/MeetTheTeam/MeetTheTeam.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react"
+import { BulletContainer, Circle, Content } from "../BulletPoint/BulletPoint.js"
+import waves from "../../../static/blue-waves-4.svg"
+
+class MeetTheTeam extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  render() {
+    return (
+      <BulletContainer>
+        <Circle />
+        <Content>{this.props.content}</Content>
+      </BulletContainer>
+    )
+  }
+}
+export default BulletPoint
