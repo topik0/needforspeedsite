d5e7786ef164debd87bcab91a43ca2a2769786af
Topik <topik@topik.tech>
2020-12-25 18:24:07 -0600

    Made a constants file with links for use in the navbar

---
+++ src/constants/NavbarLinks.js
@@ -0,0 +1,9 @@
+export const NavbarLinks = [
+  { label: "Home", url: "/" },
+  { label: "About", url: "/about" },
+  { label: "Meet The Team", url: "/meettheteam" },
+  { label: "See The Bot", url: "/seethebot" },
+  { label: "Sponsorships", url: "/sponsorships" },
+  { label: "Photos", url: "/photos" },
+]
+export default NavbarLinks
