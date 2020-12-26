995ebff1e253f3081ba072c3d3d8ee3ed703190e
Topik <topik@topik.tech>
2020-12-25 18:24:19 -0600

    Made a constants file with text for use across the site

--- src/constants/text.js
+++ src/constants/text.js
@@ -1,2 +1,5 @@
 export const MainDescription = `We are Need For Speed, the FTC robotics team from Lake Forest, Illinois`
-export const PrimaryTitle = `Coming Soon | Need For Speed Robotics`
+export const PrimaryTitle = ` | Need For Speed Robotics`
+
+export const AboutBio = `That’s a deep philosophical question which we’d love to know the answer to, but I’ll tell you about the team for now`
+export const LogoComment = `Our logo represents the peregrine falcon, the fastest animal on earth`
