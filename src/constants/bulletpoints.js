a6e626dd2a5c2f6513af8025ccd8d7f8fb614af3
Topik <topik@topik.tech>
2020-12-25 18:23:49 -0600

    Made a file that contains the the bulletpoints for the about section

---
+++ src/constants/bulletpoints.js
@@ -0,0 +1,8 @@
+export const bulletpoints = [
+    `We're from Lake Forest, Illinois`,
+    'The team have seven active members',
+    "We are currently ranked first in Illinois",
+    "We have the fastest shooter of any robot",
+    "Each member is highly skilled in their role",
+    "We are eager for greatness"
+];
