3192d3d0f8f691225eb8a521c99ae0b02b2f6acf
Topik <topik@topik.tech>
2020-12-25 18:26:36 -0600

    Modified styles for use with more components

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,12 +1,14 @@
 import styled from "styled-components"
 
 export const MainContainer = styled.div`
+  font-family: "Josefin Sans", sans-serif;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
   width: 100%;
-  height: 100vh;
-  background-color: black;
+  height: 200vh;
+  background-color: #121212;
+  z-index: -1;
 `
 export const CenteredContainer = styled.div`
   display: flex;
@@ -17,15 +19,24 @@ export const CenteredContainer = styled.div`
   width: 70%;
 `
 export const MainTitle = styled.p`
-  font-family: "Pier Sans", sans-serif;
-  font-size: 70px;
+  font-size: 105px;
   @media (orientation: portrait) {
     font-size: 60px;
   }
   color: white;
-  margin-top: 20px;
-  text-align: center;
-  font-weight: 400;
+  font-weight: 700;
+  text-shadow: 0px 0px 7px rgba(0, 0, 0, 0.25);
+`
+export const Subtitle = styled.p`
+  font-size: 45px;
+  @media (orientation: portrait) {
+    font-size: 60px;
+  }
+  color: white;
+  font-weight: 300;
+  margin-top: 10px;
+  width: 60%;
+  text-shadow: 0px 0px 7px rgba(0, 0, 0, 0.25);
 `
 export const MainLogo = styled.img`
   width: 40%;
@@ -34,4 +45,24 @@ export const MainLogo = styled.img`
   }
   height: auto;
   user-select: none;
+`
+export const FirstGradient = styled.img`
+  top: 0;
+  width: 82%;
+  height: auto;
+  position: absolute;
+  right: 0;
+  z-index: 0;
+`
+export const TextDiv = styled.div`
+  width: 80%;
+  height: 70%;
+`
+export const SplashDiv = styled.div`
+  z-index: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  height: 55vh;
+  width: 60%;
 `
