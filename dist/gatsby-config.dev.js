cc8679849bf428dcb6fd80f1fb6058a1cd55850b
Topik <topik@topik.tech>
2021-02-11 15:21:01 -0600

    Fixed json title

--- dist/gatsby-config.dev.js
+++ dist/gatsby-config.dev.js
@@ -12,7 +12,7 @@ module.exports = {
     titleTemplate: "%s - Need For Speed Robotics",
     description: "We are Need For Speed, the FTC robotics team from Lake Forest, Illinois",
     url: "https://www.needforspeedrobotics.org",
-    defaultImage: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fimg1.wikia.nocookie.net%2F__cb20140415180431%2Frunescape%2Fimages%2F8%2F84%2FGorilla.png&f=1&nofb=1",
+    image: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fimg1.wikia.nocookie.net%2F__cb20140415180431%2Frunescape%2Fimages%2F8%2F84%2FGorilla.png&f=1&nofb=1",
     twitterUsername: "team9113"
   }
 };
