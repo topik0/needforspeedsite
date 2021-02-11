2c5a7545c66326cd9970cb6b36a2d0428b86c93e
Topik <topik@topik.tech>
2021-02-11 13:13:30 -0600

    Added footer text constants

---
+++ src/constants/FooterText.js
@@ -0,0 +1,78 @@
+export const ConnectText = [
+  {
+    text: "team@needforspeedrobotics.org",
+    url: "mailto:team@needforspeedrobotics.org",
+    tooltip: "Email Us",
+    icon: "at.svg",
+    external: true,
+  },
+  {
+    text: "needforspeedrobotics.org",
+    url: "https://www.needforspeedrobotics.org",
+    tooltip: "Visit Our Site",
+    icon: "globe.svg",
+    external: true,
+  },
+  {
+    text: "YouTube",
+    url: "https://www.youtube.com/channel/UCOWBrj6dVJY2dvdL7FEDgkw",
+    tooltip: "Our YouTube Channel",
+    icon: "youtube.svg",
+    external: true,
+  },
+  {
+    text: "Twitter",
+    url: "https://www.twitter.com/team9113",
+    tooltip: "Visit Our Twitter",
+    icon: "twitter.svg",
+    external: true,
+  },
+  {
+    text: "Instagram",
+    url: "https://www.instagram.com/needforspeed9113",
+    tooltip: "Visit Our Instagram",
+    icon: "instagram.svg",
+    external: true,
+  },
+]
+export const MenuTextOne = [
+  {
+    text: "Home",
+    url: "/",
+  },
+  {
+    text: "About Us",
+    url: "/#about",
+  },
+  {
+    text: "Meet The Team",
+    url: "/#meettheteam",
+  },
+  {
+    text: "Sponsorships",
+    url: "/sponsorships",
+  },
+  {
+    text: "See The Bot",
+    url: "/soon",
+  },
+]
+export const MenuTextTwo = [
+  {
+    text: "Branding",
+    url: "/branding",
+  },
+  {
+    text: "Minecraft Server",
+    url: "/minecraft",
+  },
+  {
+    text: "Photos",
+    url: "/soon",
+  },
+  {
+    text: "Discord",
+    url: "https://discord.gg/EHbNfhpjFP",
+    external: true,
+  },
+]
