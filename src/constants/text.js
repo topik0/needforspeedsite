70f286033824f48beebb72a1cfb1e57a268cd07d
Topik <topik@topik.tech>
2021-02-11 13:14:00 -0600

    Added text constants

--- src/constants/text.js
+++ src/constants/text.js
@@ -3,3 +3,5 @@ export const PrimaryTitle = ` | Need For Speed Robotics`
 
 export const AboutBio = `That’s a deep philosophical question which we’d love to know the answer to, but I’ll tell you about the team for now`
 export const LogoComment = `Our logo represents the peregrine falcon, the fastest animal on earth`
+
+export const SponsorEmail = `mailto:team@needforspeedrobotics.org?subject=We%20are%20interested%20in%20a%20sponsorship`
