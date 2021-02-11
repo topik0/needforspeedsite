588d50121d52adc9cc8a4bde9f88b1d188d1bdec
Topik <topik@topik.tech>
2021-02-11 13:13:50 -0600

    Added sponsors text constants

---
+++ src/constants/Sponsors.js
@@ -0,0 +1,27 @@
+import boeing from "../images/sponsors/boeing.png"
+import sendcutsend from "../images/sponsors/sendcutsend.png"
+import dashflo from "../images/sponsors/dashflo.png"
+
+export const sponsors = [
+  {
+    name: "Boeing",
+    bio:
+      "Boeing is one of the largest aerospace corporations, manufacturing nearly half of all airplanes.",
+    website: "https://www.boeing.com",
+    logo: boeing,
+  },
+  {
+    name: "SendCutSend",
+    bio:
+      "SendCutSend is a part fabrication company that manufactures custom parts with wide range of materials using CNC machining, laser cutting, and other methods.",
+    website: "https://sendcutsend.com",
+    logo: sendcutsend,
+  },
+  {
+    name: "Dashflo",
+    bio:
+      "Dashflo is a quality hosting provider with servers in Texas and Germany.  Their servers run completely on AMD Ryzen CPUs.",
+    website: "https://dashflo.net/share/215",
+    logo: dashflo,
+  },
+]
