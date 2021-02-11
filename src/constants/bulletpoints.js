ad912482f5e6f2b419dcf63e63d18e4aba253074
Topik <topik@topik.tech>
2021-02-11 13:03:29 -0600

    Edited bulletpoints

--- src/constants/bulletpoints.js
+++ src/constants/bulletpoints.js
@@ -1,8 +1,7 @@
 export const bulletpoints = [
-    `We're from Lake Forest, Illinois`,
-    'The team have seven active members',
-    "We are currently ranked first in Illinois",
-    "We have the fastest shooter of any robot",
-    "Each member is highly skilled in their role",
-    "We are eager for greatness"
+    "We are among the top ranked teams in the world",
+    "We have one of the fastest ring shooters in FTC",
+    "Our robot schematics and code are publicly available",
+    "We endeavor to help our community",
+    "We actively aid teams on forums and message boards"
 ];
