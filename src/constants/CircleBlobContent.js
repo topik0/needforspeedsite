6c67dd220c54c2abccf2de8be8df14b47d11876c
Topik <topik@topik.tech>
2021-02-11 13:03:50 -0600

    Added blob content constants file

---
+++ src/constants/CircleBlobContent.js
@@ -0,0 +1,5 @@
+export const Content = [
+  { text: "See The Bot", url: "/soon", icon: "eye.svg" },
+  { text: "Sponsorships", url: "/sponsorships", icon: "sponsor.svg" },
+  { text: "Photos", url: "/soon", icon: "photo.svg" },
+]
