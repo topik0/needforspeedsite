adb5ac182a6f4e78a4d8d0aef4023eda5775412a
Topik <topik@topik.tech>
2021-02-11 13:13:37 -0600

    Added navbar text constants

--- src/constants/NavbarLinks.js
+++ src/constants/NavbarLinks.js
@@ -1,9 +1,9 @@
 export const NavbarLinks = [
   { label: "Home", url: "/" },
-  { label: "About", url: "/about" },
-  { label: "Meet The Team", url: "/meettheteam" },
-  { label: "See The Bot", url: "/seethebot" },
+  { label: "About", url: "/#about" },
+  { label: "Meet The Team", url: "/#meettheteam" },
+  { label: "See The Bot", url: "/soon" },
   { label: "Sponsorships", url: "/sponsorships" },
-  { label: "Photos", url: "/photos" },
+  { label: "Photos", url: "/soon" },
 ]
 export default NavbarLinks
