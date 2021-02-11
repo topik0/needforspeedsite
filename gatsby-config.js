cafe2ad54f64474b9ec43bdeeb92551dbdef40e8
Topik <topik@topik.tech>
2021-02-11 13:42:30 -0600

    Fixed image paths and title template

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,16 +9,16 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        icon: "src/images/favicon.png",
+        icon: "/src/images/favicon.png",
       },
     },
   ],
   siteMetadata: {
     title: "Need For Speed Robotics",
-    titleTemplate: "%s - Need For Speed Robotics",
+    titleTemplate: "%s | Need For Speed Robotics",
     description:
       "We are Need For Speed, the FTC robotics team from Lake Forest, Illinois",
     url: "https://www.needforspeedrobotics.org",
-    image: "src/images/favicon.png",
+    image: "/src/images/favicon.png",
   },
 }
