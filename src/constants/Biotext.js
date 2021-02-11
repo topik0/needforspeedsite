029294164721e6827c7cb00f0d4ef9dbc70b92c2
Topik <topik@topik.tech>
2021-02-11 13:03:01 -0600

    Added bio text constants file

---
+++ src/constants/Biotext.js
@@ -0,0 +1,48 @@
+export const members = [
+  {
+    name: "Daniel",
+    title: "Team Leader",
+    department: "Lead Programming",
+    bio:
+      "Aloha, I'm Daniel.  I've been on Need For Speed for the past three years.  I program the robot and this site, handle the social medias, outreach, and brainstorming.",
+  },
+  {
+    name: "Devin",
+    title: "Team Leader",
+    department: "Captain",
+    bio:
+      "Hi I'm Devin, and I have been on Need For Speed for three years. For the past two years I have served as team captain, managing logistics, as well as designing with CAD, building, and helping any department I can.",
+  },
+  {
+    name: "Ben",
+    title: "Team Leader",
+    department: "Lead Building",
+    bio:
+      "Hi, I am Ben. I enjoy building and improving the robot.  Improving my 3D printer to yield higher quality prints is something I like to do in a spare time.",
+  },
+  {
+    name: "Eric",
+    title: "Team Leader",
+    department: "Lead Design",
+    bio:
+      "Hello, I'm Eric! I've been a part of Need for Speed for the last three years.  While I do like to stick to working with CAD on a computer, I also do a lot of work on the actual robot, be it building, fixing, or even tuning code.",
+  },
+  {
+    name: "Sydney",
+    department: "Fundraising",
+    bio:
+      "Hi, I'm Sydney, a freshman, and I'm learning to code.  In the meantime I assist in a multitude of different areas including: recruiting, fundraising, and press releases.",
+  },
+  {
+    name: "Evan",
+    department: "Design",
+    bio:
+      "Hello, my name is Evan Haussermann and I am a CAD designer for Need For Speed. As a CAD designer, I create 3D mockups of robot parts that will be used on the robot.",
+  },
+  {
+    name: "Alex",
+    department: "Lead Marketing",
+    bio:
+      "I work on finding sponsorships with companies, as well as sourcing raw materials for use with the robot.",
+  },
+]
